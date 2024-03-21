0256a3d8c7969bb996a9640c358c6d345ccb9c24
Lucas <lucas.da_silveira@edu.univ-fcomte.fr>
2024-03-21 15:54:16 +0100

    affichage du contenu du dossier parent

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,23 +2,40 @@
 #include "ui_mainwindow.h"
 #include <QFileSystemModel>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
     // Créer un modèle pour l'arborescence des répertoires
-    QFileSystemModel *model = new QFileSystemModel(this);
-    model->setRootPath(QDir::rootPath());
+    QFileSystemModel *treeModel = new QFileSystemModel(this);
+    treeModel->setRootPath(QDir::rootPath());
 
     // Afficher l'arborescence des répertoires dans le QTreeView
-    ui->treeView->setModel(model);
-    ui->treeView->setRootIndex(model->index(QDir::rootPath()));
+    ui->treeView->setModel(treeModel);
+    ui->treeView->setRootIndex(treeModel->index(QDir::rootPath()));
     ui->treeView->setHeaderHidden(true); // Masquer l'en-tête du QTreeView
+
+    // Connecter le signal itemActivated du QTreeView à la fonction updateListView
+    connect(ui->treeView, &QTreeView::activated, this, &MainWindow::updateListView);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateListView(const QModelIndex &index)
+{
+    // Récupérer le chemin du répertoire sélectionné dans le QTreeView
+    QString path = ui->treeView->model()->data(index, QFileSystemModel::FilePathRole).toString();
+
+    // Créer un nouveau modèle pour afficher le contenu du répertoire sélectionné
+    QFileSystemModel *listModel = new QFileSystemModel(this);
+    listModel->setRootPath(path);
+
+    // Afficher le contenu du répertoire sélectionné dans le QListView
+    ui->listView->setModel(listModel);
+    ui->listView->setRootIndex(listModel->index(path));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QModelIndex>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +16,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void updateListView(const QModelIndex &index);
+
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
