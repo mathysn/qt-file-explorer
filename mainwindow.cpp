dbf1ff1573b3f2ed8b892daf375ed40add8e3b89
Lucas <lucas.da_silveira@edu.univ-fcomte.fr>
2024-03-21 15:43:32 +0100

    affichage arborescence fichier

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileSystemModel>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Créer un modèle pour l'arborescence des répertoires
+    QFileSystemModel *model = new QFileSystemModel(this);
+    model->setRootPath(QDir::rootPath());
+
+    // Afficher l'arborescence des répertoires dans le QTreeView
+    ui->treeView->setModel(model);
+    ui->treeView->setRootIndex(model->index(QDir::rootPath()));
+    ui->treeView->setHeaderHidden(true); // Masquer l'en-tête du QTreeView
 }
 
 MainWindow::~MainWindow()
